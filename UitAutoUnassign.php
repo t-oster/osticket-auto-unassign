d96c52e70937f96a9f19ea84dcced29eac1a4353
Thomas Oster <thomas.oster@upstart-it.de>
2020-11-24 12:20:40 +0100

    initial

---
+++ UitAutoUnassign.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once (INCLUDE_DIR . 'class.plugin.php');
+require_once (INCLUDE_DIR . 'class.signal.php');
+require_once (INCLUDE_DIR . 'class.app.php');
+
+class UitAutoUnassign extends Plugin {
+
+    public function bootstrap() {
+        //catch the signal from Ticket::assignToStaff
+        Signal::connect('object.edited', function($ticket, $type) {
+            if (
+                $ticket instanceof Ticket
+                && is_array($type)
+                && array_key_exists("type", $type)
+                && $type["type"] == "assigned"
+                && $ticket->getStaffId() > 0
+                && $ticket->getTeamId() > 0
+            ) {
+                if (array_key_exists("team", $type)) {
+                    //assigned to team, remove agent
+                    $ticket->setStaffId(0);
+                    //$ticket->logNote("Unassigned", "Plugin dummy: Assignment zum Agent aufgehoben: ".print_r($type, true));
+                }
+                else {
+                    //assignment ot agent, remove team
+                    $ticket->setTeamId(0);
+                    //$ticket->logNote("Unassigned", "Plugin dummy: Assignment zum Team aufgehoben: ".print_r($type, true));
+                }
+            }
+                    
+        });
+    }
+
+}
---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__file__) . '/include');
+return [
+    'upstart-it:unassign-team', # notrans
+    'version' => '1.0',
+    'name' => 'Upstart IT Unassign Team',
+    'author' => 'Upstart IT',
+    'description' => 'If a ticket gets assigned to an agent, unassign the team and vice versa',
+    'url' => 'https://github.com/t-oster/osticket-auto-unassign',
+    'plugin' => 'UitAutoUnassign.php:UitAutoUnassign'
+];
