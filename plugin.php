5baf91dd9417b262091ab5b7a8f6a5b436a8f438
Thomas Oster <thomas.oster@upstart-it.de>
2020-11-24 12:34:50 +0100

    update name, cleanup

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__file__) . '/inc
 return [
     'upstart-it:unassign-team', # notrans
     'version' => '1.0',
-    'name' => 'Upstart IT Unassign Team',
+    'name' => 'Upstart IT Auto Unassign',
     'author' => 'Upstart IT',
     'description' => 'If a ticket gets assigned to an agent, unassign the team and vice versa',
     'url' => 'https://github.com/t-oster/osticket-auto-unassign',
