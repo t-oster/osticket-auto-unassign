be0395eadde8af5b7e63e4f943cfc91343a326a9
Angel Andrades <99033842+angelandasi@users.noreply.github.com>
2022-10-28 19:18:31 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,10 +3,10 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__file__) . '/include');
 return [
     'upstart-it:unassign-team', # notrans
-    'version' => '1.0',
-    'name' => 'Upstart IT Auto Unassign',
-    'author' => 'Upstart IT',
+    'version' => '1.0 edited',
+    'name' => 'Upstart IT Auto Unassign Edited',
+    'author' => 'Upstart IT - edited by Angel Andrades',
     'description' => 'If a ticket gets assigned to an agent, unassign the team and vice versa',
     'url' => 'https://github.com/t-oster/osticket-auto-unassign',
-    'plugin' => 'UitAutoUnassign.php:UitAutoUnassign'
+    'plugin' => 'UitAutoUnassignEdited.php:UitAutoUnassignEdited'
 ];
