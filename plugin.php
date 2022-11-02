399bd73c995fc78e236d28c275f2a743eefbcb5e
Angel Andrades <99033842+angelandasi@users.noreply.github.com>
2022-11-02 08:19:51 +0100

    Works with osTicket v1.17, plus additional option (#3)
    Allow to work with osTicket v1.17 and adding the option to choose between "Assigning only to Team or Agent" or "Release Agent when assigning to another Team". With this second option we can have both Team and Agent assigned.

--- plugin.php
+++ plugin.php
@@ -3,9 +3,9 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__file__) . '/include');
 return [
     'upstart-it:unassign-team', # notrans
-    'version' => '1.0',
+    'version' => '1.1',
     'name' => 'Upstart IT Auto Unassign',
-    'author' => 'Upstart IT',
+    'author' => 'Upstart IT - edited by Angel Andrades',
     'description' => 'If a ticket gets assigned to an agent, unassign the team and vice versa',
     'url' => 'https://github.com/t-oster/osticket-auto-unassign',
     'plugin' => 'UitAutoUnassign.php:UitAutoUnassign'
